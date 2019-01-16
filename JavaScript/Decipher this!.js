46c9323a3a5f5d0bad6b774e99dde599e4224d0a
Julien Shim <25723428+julienshim@users.noreply.github.com>
2019-01-16 15:10:09 -0800

    Update Decipher this!.js
    a little more dry. needs to be relabeled.

--- JavaScript/Decipher this!.js
+++ JavaScript/Decipher this!.js
@@ -1,15 +1,11 @@
 function decipherThis(str) {
 //have fun!
   let split = str.split(" ");
-  let newSplit = [];
   let deciphered = [];
   split.forEach(function(test) {
     let number = test.match(/\d/g).join("");
     let newTest = test.replace(number, String.fromCharCode(number));
-    newSplit.push(newTest);
-  });
-  newSplit.forEach(function(swappie){
-    let splittie = swappie.split("");
+    let splittie = newTest.split("");
     let tmp = splittie[1];
     splittie[1] = splittie[splittie.length-1];
     splittie[splittie.length-1] = tmp;

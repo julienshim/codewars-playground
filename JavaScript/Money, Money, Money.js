d69a42441b975c59759299c5aa562e574199e8b5
Julien Shim <25723428+julienshim@users.noreply.github.com>
2019-03-22 10:18:16 -0700

    Create Money, Money, Money.js

---
+++ JavaScript/Money, Money, Money.js
@@ -0,0 +1,12 @@
+function calculateYears(principal, interest, tax, desired) {
+    // your code
+    let years = 0
+    
+    while (principal < desired) {
+      let ir = principal * interest;
+      let tr = principal * interest * tax;
+      principal = principal + ir - tr;
+      years++;
+    }
+    
+    return years

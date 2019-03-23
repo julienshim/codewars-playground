bc34c05375c327d721e190a57877bd6e062c4e57
Julien Shim <25723428+julienshim@users.noreply.github.com>
2019-03-22 20:57:27 -0700

    Create Counting Duplicates.js

---
+++ JavaScript/Counting Duplicates.js
@@ -0,0 +1,19 @@
+function duplicateCount(text){
+  //...
+  let duplicates = 0;
+  let store = {};
+  for (let i = 0; i < text.length; i++) {
+    let lc = String(text[i]) ? text[i].toLowerCase() : text[i];
+    if (store[lc] === undefined) {
+      store[lc] = 1;
+    } else {
+      store[lc]++
+    }
+  }
+  for (let i in store) {
+    if (store[i] > 1) {
+      duplicates++;
+    }
+  }
+  return duplicates;
+}

b70b76386a842532dc44b001a43db12eeba02f56
Julien Shim <25723428+julienshim@users.noreply.github.com>
2019-03-27 15:25:12 -0700

    Create Basic Mathematical Operations.py

---
+++ Python/Basic Mathematical Operations.py
@@ -0,0 +1,9 @@
+def basic_op(operator, value1, value2):
+    switcher = {
+        '+': value1 + value2,
+        '-': value1 - value2,
+        '*': value1 * value2,
+        '/': value1 / value2,
+    }
+  
+    return switcher.get(operator, "Invalid Operator") 

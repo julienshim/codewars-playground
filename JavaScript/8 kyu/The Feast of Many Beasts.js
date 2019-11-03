00c39cbd124e6ee7175418456348009a8a18865a
Julien Shim <25723428+julienshim@users.noreply.github.com>
2019-11-02 18:17:10 -0700

    Create The Feast of Many Beasts.js

---
+++ JavaScript/8 kyu/The Feast of Many Beasts.js
@@ -0,0 +1,10 @@
+function feast(beast, dish) {
+//your function here
+  
+  return splice(beast) === splice(dish);
+  
+  function splice(word) {
+    return word[0] + word[word.length - 1];
+  }
+  
+}

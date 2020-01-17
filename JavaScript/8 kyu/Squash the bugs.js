9fa36cd5d5e6343511eaf3013447101a937c6356
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-01-16 20:43:40 -0800

    squash

---
+++ JavaScript/8 kyu/Squash the bugs.js
@@ -0,0 +1,10 @@
+function findLongest(str) {
+  const spl = str.split(" ");
+  let longest = 0
+  for (let i = 0; i < spl.length; i++) {
+    if (spl[i].length > longest) (
+      longest = spl[i].length
+    )
+  }
+  return longest
+}

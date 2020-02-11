ceb2c75b8765d366976e345950da517490210f77
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-02-11 05:43:26 -0800

    added beg ser -2 clock

---
+++ JavaScript/8 kyu/Beginner Series -2 Clock.js
@@ -0,0 +1,14 @@
+function past(h, m, s){
+  //#Happy Coding! ^_^
+  let seconds = 0;
+  if (h > 0) {
+    seconds += h * 3600;
+  }
+  if (m > 0) {
+    seconds += m * 60;
+  }
+  if (s > 0) {
+    seconds += s;
+  }
+  return seconds * 1000;
+}

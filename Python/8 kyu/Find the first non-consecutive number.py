a2a2fe7b3861a884fad93203d1171cc2781efb1e
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-03-09 09:14:01 -0700

    Create Find the first non-consecutive number.py

---
+++ Python/8 kyu/Find the first non-consecutive number.py
@@ -0,0 +1,8 @@
+def first_non_consecutive(arr):
+    #your code here
+    consec = arr[0]
+    for num in arr:
+        if num != consec:
+            return(num)
+        consec += 1
+    return None

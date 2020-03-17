40cf7550d79223fd239d4af7191e368b61ef0e55
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-03-16 23:27:43 -0700

    Create Triple Trouble.js

---
+++ JavaScript/8 kyu/Triple Trouble.js
@@ -0,0 +1,11 @@
+function tripleTrouble(one, two, three){
+  //Solution
+  let string = ""
+  
+  for(let i = 0; i < one.length; i++){
+    string += one[i]+two[i]+three[i];
+  }
+  
+  return string
+  
+ }

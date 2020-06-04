640b6021742c8c50a38ad714f82a51f00e4bfe5c
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-06-03 21:49:03 -0700

    welcome

---
+++ Python/8 kyu/Welcome-.py
@@ -0,0 +1,22 @@
+def greet(lang):
+    languages = {
+    'english': 'Welcome',
+    'czech': 'Vitejte',
+    'danish': 'Velkomst',
+    'dutch': 'Welkom',
+    'estonian': 'Tere tulemast',
+    'finnish': 'Tervetuloa',
+    'flemish': 'Welgekomen',
+    'french': 'Bienvenue',
+    'german': 'Willkommen',
+    'irish': 'Failte',
+    'italian': 'Benvenuto',
+    'latvian': 'Gaidits',
+    'lithuanian': 'Laukiamas',
+    'polish': 'Witamy',
+    'spanish': 'Bienvenido',
+    'swedish': 'Valkommen',
+    'welsh': 'Croeso'
+    }
+    return languages[lang] if lang in languages.keys() else languages['english']
+      

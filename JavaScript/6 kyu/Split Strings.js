336d5abe542d432623380390cc8216ca0f856513
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-06-20 20:25:25 -0700

    solve 2

--- JavaScript/6 kyu/Split Strings.js
+++ JavaScript/6 kyu/Split Strings.js
@@ -1,3 +1,9 @@
 function solution(str){
   return str ? (str + "_").match(/../g) : [];
 }
+
+// solve 2
+// function solution(str){
+//   const even = str.length % 2 === 0 ? str : str += '_';
+//   return str ? str.match(/[a-z_]{2,2}/gi) : [];
+// }

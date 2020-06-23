158effc42758a6d769c7d3bf31138982605d1584
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-06-22 17:40:42 -0700

    Update Regex count lowercase letters.py

--- Python/8 kyu/Regex count lowercase letters.py
+++ Python/8 kyu/Regex count lowercase letters.py
@@ -1,3 +1,11 @@
+# def lowercase_count(strng):
+#     # Your code here
+#     return [s.lower() == s and s.isalpha() for s in strng].count(True) # could be changed to islower
+
+# proper regex version
+
+import re
+
 def lowercase_count(strng):
     # Your code here
-    return [s.lower() == s and s.isalpha() for s in strng].count(True)
+    return len(re.findall('[a-z]', strng))

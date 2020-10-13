de2b9079d813623c3520c34ee3a1739aa437e211
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-10-12 22:16:43 -0700

    updated solve awkward wording

--- Python/8 kyu/Rock Paper Scissors-.py
+++ Python/8 kyu/Rock Paper Scissors-.py
@@ -1,19 +1,16 @@
 def rps(p1, p2):
-    #your code here
+    # your code here    # your code here
     winner = 2
-    
+
     fight = {
         "rock": "scissors",
         "scissors": "paper",
         "paper": "rock"
     }
-    
+
     if p1 == p2:
         return "Draw!"
-    else:
-        for combos in fight: 
-            if fight[p1] == p2:
-                winner = 1
-                
+    elif fight[p1] == p2:
+        winner = 1
+
     return f"Player {winner} won!"
-            

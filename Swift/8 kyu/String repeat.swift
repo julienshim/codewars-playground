2ef5147ab141dba20387479e753ba4131c18098b
julienshim <25723428+julienshim@users.noreply.github.com>
2022-02-27 19:25:01 -0800

    sol 2

--- Swift/8 kyu/String repeat.swift
+++ Swift/8 kyu/String repeat.swift
@@ -1,8 +1,13 @@
+// func repeatStr(_ n: Int, _ string: String) -> String {
+//   // Code here:
+//   var a = ""
+//   for i in 0..<n {
+//     a = a + string
+//   }
+//   return a
+// }
+
 func repeatStr(_ n: Int, _ string: String) -> String {
   // Code here:
-  var a = ""
-  for i in 0..<n {
-    a = a + string
-  }
-  return a
+  return String(repeating: string, count: n)
 }

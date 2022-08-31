ddce1b5c1ebed21f42c833d1403b11710911bec6
julienshim <25723428+julienshim@users.noreply.github.com>
2022-08-30 22:13:16 -0700

    bmo

---
+++ JavaScript/8 kyu/Basic Mathematical Operations.js
@@ -0,0 +1,16 @@
+function basicOp(operation, value1, value2)
+{
+  switch(operation) {
+        case '+': 
+          return value1 + value2;
+          pass;
+        case '-': 
+          return value1 - value2;
+          pass;
+        case '*': 
+          return value1 * value2;
+          pass;
+        case '/': 
+          return value1 / value2;
+  }
+}

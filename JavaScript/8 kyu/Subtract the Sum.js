6f444aa3612821a8001e8c322804ef527b47fc8e
julienshim <25723428+julienshim@users.noreply.github.com>
2022-12-07 23:08:32 -0800

    sts

---
+++ JavaScript/8 kyu/Subtract the Sum.js
@@ -0,0 +1,33 @@
+function SubtractSum(n){
+  
+  const fruits = {
+    1: "kiwi",
+    2: "pear",
+    3: "kiwi",
+    4: "banana",
+    5: "melon",
+    6: "banana",
+    7: "melon",
+    8: "pineapple",
+    9: "apple",
+    10: "pineapple",
+    11: "cucumber",
+    12: "pineapple",
+    13: "cucumber",
+    14: "orange",
+    15: "grape",
+    16: "orange",
+    17: "grape",
+    18: "apple",
+    19: "grape",
+    20: "cherry",
+    21: "pear",
+    22: "cherry"
+  }
+  
+  const sum = x => String(x).split('').reduce((a, c) => a + +c, 0);
+  
+  const newN = n - sum(n)
+  
+  return newN in fruits ? fruits[newN] : SubtractSum(newN) // fruit name like "apple"
+}

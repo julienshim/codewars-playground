690f7bcfd79938586ef8e05ffd6262c4c4d4d8dd
julienshim <25723428+julienshim@users.noreply.github.com>
2022-12-08 23:45:57 -0800

    sts

--- TypeScript/8 kyu/Subtract the Sum.ts
+++ TypeScript/8 kyu/Subtract the Sum.ts
@@ -1,28 +1,34 @@
-def subtract_sum(number):
-    fruits = {
-        1: "kiwi",
-        2: "pear",
-        3: "kiwi",
-        4: "banana",
-        5: "melon",
-        6: "banana",
-        7: "melon",
-        8: "pineapple",
-        9: "apple",
-        10: "pineapple",
-        11: "cucumber",
-        12: "pineapple",
-        13: "cucumber",
-        14: "orange",
-        15: "grape",
-        16: "orange",
-        17: "grape",
-        18: "apple",
-        19: "grape",
-        20: "cherry",
-        21: "pear",
-        22: "cherry"
-    }   
-
-    new_n = number - sum(int(n) for n in str(number))
-    return fruits[new_n] if new_n in fruits else subtract_sum(new_n) #fruit name like "apple"
+export function subtractSum(n: number): string {
+    // fruit name like "apple"
+      
+    const fruits: any = {
+      1: "kiwi",
+      2: "pear",
+      3: "kiwi",
+      4: "banana",
+      5: "melon",
+      6: "banana",
+      7: "melon",
+      8: "pineapple",
+      9: "apple",
+      10: "pineapple",
+      11: "cucumber",
+      12: "pineapple",
+      13: "cucumber",
+      14: "orange",
+      15: "grape",
+      16: "orange",
+      17: "grape",
+      18: "apple",
+      19: "grape",
+      20: "cherry",
+      21: "pear",
+      22: "cherry"
+    }
+    
+    const sum = (x: number) => String(x).split('').reduce((a, c) => a + +c, 0);
+    
+    const newN = n - sum(n)
+    
+    return newN in fruits ? fruits[newN] : subtractSum(newN) // fruit name like "apple"
+  }

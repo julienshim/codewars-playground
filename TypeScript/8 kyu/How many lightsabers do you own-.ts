6e4ac7a971e84bf6325bcdc8858b034bb2d78c11
julienshim <25723428+julienshim@users.noreply.github.com>
2022-12-24 23:16:40 -0800

    hmldyo

---
+++ TypeScript/8 kyu/How many lightsabers do you own-.ts
@@ -0,0 +1,5 @@
+export function howManyLightsabersDoYouOwn(name?: any): number {
+    return name === 'Zach' ? 18 : 0; // So, how many?
+}
+
+// The question mark (?) after name attribute marks it as optional.

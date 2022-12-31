9df0a323755a79af861310b2e36460199349f8e5
julienshim <25723428+julienshim@users.noreply.github.com>
2022-12-30 23:16:58 -0800

    cb

---
+++ JavaScript/8 kyu/Calculate BMI.js
@@ -0,0 +1,12 @@
+function bmi(weight, height) {
+    const mass = weight / Math.pow(height, 2)
+    if (mass <= 18.5) {
+      return "Underweight"
+    } else if (mass <= 25.0) {
+      return "Normal"
+    } else if (mass <= 30.0) {
+      return "Overweight"
+    } else {
+      return "Obese"
+    }
+}
